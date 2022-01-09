71eff0c090d88e025411a2ee0f83aef6dec1b551
钰涛 徐 <2485552681@qq.com>
2022-01-09 23:06:40 +0800

    Update

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ErenChris_TiCloud.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
